001199e45e79aa48b3d9f37e701524035a4ff8f2
jesus <jesus@MAXI>
2019-04-12 15:59:08 -0300

    Testing new branch.

--- src/Vistas/Main.java
+++ src/Vistas/Main.java
@@ -34,7 +34,7 @@ public class Main {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(99, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 	
