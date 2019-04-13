e58d68c5dc30269b2155920a9a195ccde976a8e0
faku6 <faku6@DESKTOP-7I6F6EP>
2019-04-13 14:54:59 -0300

    Modificacion de la ventana Pincipal. Agragado del Menu-Opciones

--- src/Formularios/frmPrincipal.java
+++ src/Formularios/frmPrincipal.java
@@ -3,6 +3,17 @@ package Formularios;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import java.awt.BorderLayout;
+import javax.swing.JButton;
+import javax.swing.JDesktopPane;
+import javax.swing.JMenuBar;
+import javax.swing.JComboBox;
+import javax.swing.JMenuItem;
+import javax.swing.JMenu;
+import javax.swing.JCheckBoxMenuItem;
+import javax.swing.SwingConstants;
+import java.awt.Font;
 
 public class frmPrincipal {
 
@@ -25,8 +36,71 @@ public class frmPrincipal {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 711, 496);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JMenuBar menuBar = new JMenuBar();
+		menuBar.setFont(new Font("Segoe UI Emoji", Font.PLAIN, 23));
+		menuBar.setEnabled(false);
+		frame.setJMenuBar(menuBar);
+		
+		JMenu mnVentas = new JMenu("Ventas");
+		mnVentas.setFont(new Font("Segoe UI Emoji", Font.PLAIN, 14));
+		mnVentas.setHorizontalAlignment(SwingConstants.CENTER);
+		menuBar.add(mnVentas);
+		
+		JMenuItem mntmRealizarVenta = new JMenuItem("Realizar Venta");
+		mntmRealizarVenta.setFont(new Font("Segoe UI Emoji", Font.PLAIN, 14));
+		mnVentas.add(mntmRealizarVenta);
+		
+		JMenuItem mntmVentasRealizadas = new JMenuItem("Ventas Realizadas");
+		mntmVentasRealizadas.setFont(new Font("Segoe UI Emoji", Font.PLAIN, 14));
+		mnVentas.add(mntmVentasRealizadas);
+		
+		JMenu mnProductos = new JMenu("Productos");
+		mnProductos.setFont(new Font("Segoe UI Emoji", Font.PLAIN, 14));
+		menuBar.add(mnProductos);
+		
+		JMenuItem mntmAltaDeProductos = new JMenuItem("Alta de Productos");
+		mntmAltaDeProductos.setFont(new Font("Segoe UI Emoji", Font.PLAIN, 14));
+		mnProductos.add(mntmAltaDeProductos);
+		
+		JMenu mnTarjetas = new JMenu("Tarjetas");
+		mnTarjetas.setFont(new Font("Segoe UI Emoji", Font.PLAIN, 14));
+		mnTarjetas.setHorizontalAlignment(SwingConstants.CENTER);
+		menuBar.add(mnTarjetas);
+		
+		JMenuItem mntmAltasDeTarjetas = new JMenuItem("Altas de Tarjetas");
+		mntmAltasDeTarjetas.setFont(new Font("Segoe UI Emoji", Font.PLAIN, 14));
+		mnTarjetas.add(mntmAltasDeTarjetas);
+		
+		JMenuItem mntmTarjetasActivas = new JMenuItem("Tarjetas Activas");
+		mntmTarjetasActivas.setFont(new Font("Segoe UI Emoji", Font.PLAIN, 14));
+		mnTarjetas.add(mntmTarjetasActivas);
+		
+		JMenuItem mntmPedidoDeTarjetas = new JMenuItem("Pedido de Tarjetas");
+		mntmPedidoDeTarjetas.setFont(new Font("Segoe UI Emoji", Font.PLAIN, 14));
+		mnTarjetas.add(mntmPedidoDeTarjetas);
+		
+		JMenu mnClientes = new JMenu("Clientes");
+		mnClientes.setFont(new Font("Segoe UI Emoji", Font.PLAIN, 14));
+		menuBar.add(mnClientes);
+		
+		JMenuItem mntmAltaDeClientes = new JMenuItem("Alta de Clientes");
+		mntmAltaDeClientes.setFont(new Font("Segoe UI Emoji", Font.PLAIN, 14));
+		mnClientes.add(mntmAltaDeClientes);
+		
+		JMenuItem mntmClientesInactivos = new JMenuItem("Clientes Inactivos");
+		mntmClientesInactivos.setFont(new Font("Segoe UI Emoji", Font.PLAIN, 14));
+		mnClientes.add(mntmClientesInactivos);
+		
+		JMenu mnUsuarios = new JMenu("Usuarios");
+		mnUsuarios.setFont(new Font("Segoe UI Emoji", Font.PLAIN, 14));
+		menuBar.add(mnUsuarios);
+		
+		JMenuItem mntmAdminUsuarios = new JMenuItem("Admin Usuarios");
+		mntmAdminUsuarios.setFont(new Font("Segoe UI Emoji", Font.PLAIN, 14));
+		mnUsuarios.add(mntmAdminUsuarios);
 	}
 	
 	public void mostrarPrincipal() {
