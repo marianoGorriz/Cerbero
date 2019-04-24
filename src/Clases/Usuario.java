2e9c37ae3993335c3803f4fe713876a5d58bf45b
faku6 <faku6@DESKTOP-7I6F6EP>
2019-04-24 00:36:19 -0300

    gg

--- src/Clases/Usuario.java
+++ src/Clases/Usuario.java
@@ -11,7 +11,7 @@ import Vistas.Conexion;
 public class Usuario extends Persona {
 	
 	private String usuario, contraseña, rol;
-	private int id_personas_usuario;
+	private int id_personas_usuario,id;
 	
 	public int validacionLogin(String usuario, String password, int bandera) 
 	{
@@ -35,6 +35,51 @@ public class Usuario extends Persona {
 		
 		
 	}
+	
+	public void altaUsuario(String usuario, String contraseña, String rol) {
+				
+		try {
+			Conexion cn = new Conexion();
+			Connection con = cn.conectarDB();
+			Statement st =con.createStatement();
+			st.executeUpdate("INSERT INTO usuarios (`usuario`, `contraseña`,`rol`, estado) VALUES ('" + usuario + "', '" + contraseña + "', '"+rol+"');");
+			
+		}catch(SQLException e2) {
+			System.out.println("nope2");
+			System.out.println("SQLException: " + e2.getMessage());		
+			
+		}
+	}
+	public ResultSet buscarUsuario(String usuario) {
+		try {
+			Conexion cn = new Conexion();
+			Connection con = cn.conectarDB();
+			Statement st =con.createStatement();
+		    ResultSet rs = st.executeQuery("SELECT * FROM usuarios WHERE usuario LIKE '%" + usuario +"%';");
+			return rs;
+
+		}		 
+		catch (SQLException e2) {
+			System.out.println("nope2");
+			System.out.println("SQLException: " + e2.getMessage());
+		}
+		return null;
+	}
+	public void modificarUsuario(String usuario, String contraseña, int id, String rol) {
+		try {
+			Conexion cn = new Conexion();
+			Connection con = cn.conectarDB();
+			Statement st =con.createStatement();
+			st.executeUpdate("UPDATE usuarios SET `usuario` = '" + usuario + "', `contraseña` = '" + contraseña + 
+							 "',`rol` = '" + rol + "' WHERE id = " + id + ";");
+			
+		}		 
+		catch (SQLException e2) {
+			System.out.println("nope2");
+			System.out.println("SQLException: " + e2.getMessage());
+		}
+	}
+    
 	
 
 }
--- src/Formularios/PanelUsuarios.java
+++ src/Formularios/PanelUsuarios.java
@@ -12,12 +12,21 @@ import javax.swing.JSpinner;
 import javax.swing.JComboBox;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
+
+import Clases.Producto;
+import Clases.Usuario;
+
 import java.awt.event.ActionListener;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.awt.event.ActionEvent;
 import javax.swing.JPasswordField;
 import java.awt.Choice;
 import java.awt.List;
 import java.awt.ComponentOrientation;
+import javax.swing.JScrollPane;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class PanelUsuarios extends JPanel {
 	private JTextField textField;
@@ -35,37 +44,37 @@ public class PanelUsuarios extends JPanel {
 		
 		JLabel lblIdDelUsuario = new JLabel("Id del Usuario:");
 		lblIdDelUsuario.setFont(new Font("Tw Cen MT Condensed", Font.BOLD, 13));
-		lblIdDelUsuario.setBounds(33, 50, 82, 14);
+		lblIdDelUsuario.setBounds(29, 26, 82, 14);
 		add(lblIdDelUsuario);
 		
-		JLabel lblNombre = new JLabel("Nombre:");
+		JLabel lblNombre = new JLabel("Usuario:");
 		lblNombre.setFont(new Font("Tw Cen MT Condensed", Font.BOLD, 13));
-		lblNombre.setBounds(33, 84, 82, 14);
+		lblNombre.setBounds(29, 60, 82, 14);
 		add(lblNombre);
 		
 		JLabel lblContrasea = new JLabel("Contrase\u00F1a:");
 		lblContrasea.setFont(new Font("Tw Cen MT Condensed", Font.BOLD, 13));
-		lblContrasea.setBounds(33, 120, 82, 14);
+		lblContrasea.setBounds(29, 96, 82, 14);
 		add(lblContrasea);
 		
 		JLabel lblRepetirContrasea = new JLabel("Repetir Contrase\u00F1a:");
 		lblRepetirContrasea.setFont(new Font("Tw Cen MT Condensed", Font.BOLD, 13));
-		lblRepetirContrasea.setBounds(33, 156, 98, 14);
+		lblRepetirContrasea.setBounds(29, 132, 98, 14);
 		add(lblRepetirContrasea);
 		
 		JLabel lblRol = new JLabel("Rol:");
 		lblRol.setFont(new Font("Tw Cen MT Condensed", Font.BOLD, 13));
-		lblRol.setBounds(33, 190, 82, 14);
+		lblRol.setBounds(29, 166, 82, 14);
 		add(lblRol);
 		
 		textField = new JTextField();
-		textField.setBounds(158, 47, 130, 20);
+		textField.setBounds(154, 23, 130, 20);
 		add(textField);
 		textField.setColumns(10);
 		
 		textField_1 = new JTextField();
 		textField_1.setColumns(10);
-		textField_1.setBounds(158, 81, 130, 20);
+		textField_1.setBounds(154, 57, 130, 20);
 		add(textField_1);
 		
 		JList list = new JList();
@@ -73,52 +82,167 @@ public class PanelUsuarios extends JPanel {
 		add(list);
 		
 		textField_4 = new JTextField();
-		textField_4.setBounds(83, 221, 177, 20);
+		textField_4.setBounds(79, 197, 177, 20);
 		add(textField_4);
 		textField_4.setColumns(10);
 		
-		JButton btnBuscar = new JButton("Buscar");
-		btnBuscar.setBounds(300, 221, 89, 23);
-		add(btnBuscar);
 		
-		JButton btnNuevo = new JButton("Nuevo");
-		btnNuevo.setBounds(410, 46, 104, 23);
-		add(btnNuevo);
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(25, 239, 489, 155);
+		add(scrollPane);
+		table = new JTable();
 		
-		JButton btnActualizar = new JButton("Actualizar");
-		btnActualizar.setBounds(410, 80, 104, 23);
-		add(btnActualizar);
+		JButton btnHabilitar = new JButton("Habilitar");
+		btnHabilitar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnHabilitar.setBounds(410, 132, 104, 23);
+		add(btnHabilitar);
 		
-		JButton btnEliminar = new JButton("Eliminar");
-		btnEliminar.setBounds(410, 116, 104, 23);
+		JButton btnEliminar = new JButton("Desabilitar");
+		btnEliminar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnEliminar.setBounds(410, 96, 104, 23);
 		add(btnEliminar);
 		
-		JButton btnCancelar = new JButton("Cancelar");
-		btnCancelar.setBounds(410, 152, 104, 23);
-		add(btnCancelar);
+		
+		Choice choice = new Choice();
+		choice.setBounds(154, 160, 130, 20);
+		add(choice);
+		choice.add("Administrador");
+		choice.add("Moza");
+		
+		JButton btnActualizar = new JButton("Actualizar");
+		btnActualizar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				int column = 0;
+				int row = table.getSelectedRow();
+
+				int id = Integer.parseInt(table.getModel().getValueAt(row, column).toString());
+				Usuario usuario= new Usuario();
+				char [] array = passwordField.getPassword();
+				String pass = new String(array);
+				usuario.modificarUsuario(textField_1.getText(), pass, id, choice.getSelectedItem());
+				
+				//btnActualizar.setText("Nuevo");
+				//textField.setText("");
+				//textField_1.setText("");
+			}
+		});
+		
+		btnActualizar.setBounds(410, 60, 104, 23);
+		add(btnActualizar);
+		
+		table.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				int column1 = 0;
+				int column2 = 1;
+				int column3 = 2;
+				int column4 = 3;
+				int column5 = 4;
+				btnActualizar.setText("Actualizar");
+				int row = table.getSelectedRow();
+				String value1 = table.getModel().getValueAt(row, column1).toString();
+				String value2 = table.getModel().getValueAt(row, column2).toString();
+				String value3 = table.getModel().getValueAt(row, column3).toString();
+				String value4 = table.getModel().getValueAt(row, column4).toString();
+				String value5 = table.getModel().getValueAt(row, column5).toString();
+
+				textField.setText(value1);
+				textField_1.setText(value2);
+				passwordField.setText(value3);
+				choice.select(value4);
+				choice.repaint();
+				
+				
+				if(value5 == "true") {
+					btnEliminar.setText("Deshabilitar");
+					btnEliminar.setEnabled(true);
+				}else {
+					btnHabilitar.setText("Habilitar");
+					btnHabilitar.setEnabled(true);
+				}
+				
+				
+			}
+		});
+		
+		
+		
+		scrollPane.setViewportView(table);
+		table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+		JButton btnBuscar = new JButton("Buscar");
+		
+		btnBuscar.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					DefaultTableModel modelo;
+					modelo = new DefaultTableModel();
+					modelo.addColumn("ID");
+					modelo.addColumn("Usuario");
+					modelo.addColumn("Contraseña");
+					modelo.addColumn("Rol");
+					modelo.addColumn("Estado");
+					table.setModel(modelo);
+					modelo.addRow(new Object[]{"ID", "Usuario", "Contraseña", "Rol","Estado"});
+					ResultSet rs;
+					Usuario usuario = new Usuario();
+					rs = usuario.buscarUsuario(textField_4.getText());
+					try {
+						while(rs.next()) {
+							modelo.addRow(new Object[]{rs.getObject("id"), rs.getObject("usuario"), rs.getObject("contraseña"), rs.getObject("rol"), rs.getObject("Estado")});
+						}
+					} catch (SQLException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+				}
+			
+		});
+		btnBuscar.setBounds(296, 197, 89, 23);
+		add(btnBuscar);
+		
+
+
+		
+		JButton btnNuevo = new JButton("Nuevo");
+		btnNuevo.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Usuario usuario = new Usuario();
+				
+				
+					char [] array = passwordField.getPassword();
+					String pass = new String(array);
+					usuario.altaUsuario(textField_1.getText(), pass, choice.getSelectedItem() );
+					
+				
+					
+				
+			}
+		});
+		btnNuevo.setBounds(410, 26, 104, 23);
+		add(btnNuevo);
+		
+		
+		
+
 		
 		passwordField = new JPasswordField();
-		passwordField.setBounds(158, 117, 130, 20);
+		passwordField.setBounds(154, 93, 130, 20);
 		add(passwordField);
 		
 		passwordField_1 = new JPasswordField();
-		passwordField_1.setBounds(158, 153, 130, 20);
+		passwordField_1.setBounds(154, 129, 130, 20);
 		add(passwordField_1);
 		
-		Choice choice = new Choice();
-		choice.setBounds(158, 184, 130, 20);
-		add(choice);
 		
-		table = new JTable();
-		table.setModel(new DefaultTableModel(
-			new Object[][] {
-			},
-			new String[] {
-				"ID", "Rol", "Contrase\u00F1a", "Nombre"
-			}
-		));
-		table.setBounds(33, 414, 502, -129);
-		add(table);
+		
+		
+		
+
 
 	}
 }
