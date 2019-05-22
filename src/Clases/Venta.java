50fa805b02f5c50e04fafda3c53e9d2ae52c7d92
faku6 <faku6@DESKTOP-7I6F6EP>
2019-05-22 00:28:35 -0300

    Cambios en la clase ventas

--- src/Clases/Venta.java
+++ src/Clases/Venta.java
@@ -1,5 +1,14 @@
 package Clases;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.swing.JOptionPane;
+
+import Vistas.Conexion;
+
 public class Venta extends Producto {
 	
 	private int id_ventas_usuarios, id_ventas_tarjetas, total_puntos, cantidad;
@@ -37,7 +46,23 @@ public class Venta extends Producto {
 	public void setCantidad(int cantidad) {
 		this.cantidad = cantidad;
 	}
-	
+	/*public ResultSet VentasRealizadas () {
+		try{ 
+			Conexion cn = new Conexion();
+			Connection con = cn.conectarDB();
+			Statement st = con.createStatement();
+			ResultSet rs = st.executeQuery( "SELECT * FROM clientes INNER JOIN tarjetas ON clientes.id = tarjetas.id_tarjeta_cliente WHERE tarjetas.n_tarjeta LIKE '%" + nro_tarjeta + "%' AND tarjetas.estado = 0;");
+			return rs;
+		}
+		catch (SQLException e2) {
+			//System.out.println("nope2");
+			JOptionPane.showMessageDialog(null, "Error");
+			System.out.println("SQLException: " + e2.getMessage());
+			
+	}
+		return null;
+		
+	}*/
 	
 
 }
