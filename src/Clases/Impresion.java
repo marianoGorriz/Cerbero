952514ca7cba4d85d9d9c271e2ef9b0f6a90701e
mariano <mariano@DESKTOP-GK37NMM>
2019-06-13 19:35:17 -0300

    imp final

--- src/Clases/Impresion.java
+++ src/Clases/Impresion.java
@@ -11,13 +11,14 @@ public class Impresion {
 
         try {
 
-            FileWriter fw = new FileWriter("gg.txt");
+            FileWriter fw = new FileWriter("C:\\Users\\mariano\\Desktop\\ggx2.docx");
 
             PrintWriter pw = new PrintWriter(fw);
-
+            		
             pw.print("CERBERO RESTOBAR");
             pw.print("\r\n");
             pw.print("CUIL: 202020");
+            pw.print("\r\n");
             pw.print("\r\n");
             if(tipo == 0) {
                 pw.print(nombre + " " + apellido);
@@ -43,7 +44,7 @@ public class Impresion {
                 pw.print("\r\n");
             }
 
-            File fileToPrint = new File("gg.txt");
+            File fileToPrint = new File("C:\\\\Users\\\\mariano\\\\Desktop\\\\ggx2.docx");
             Desktop.getDesktop().print(fileToPrint);
 
             pw.close();
